c9c1569edc60ff6b3ac0026315ad528eb711aa33
Vismaya T Surendran <vismayatsurendran@gmail.com>
2018-06-25 19:39:15 +0530

    Updated the shelf

--- src/BooksShelf.js
+++ src/BooksShelf.js
@@ -1,3 +1,32 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import BookList from './BookList';
+
+class BooksShelf extends Component{	
+	static propTypes={
+		books: PropTypes.array.isRequired,
+		updateShelf: PropTypes.func.isRequired,
+		title: PropTypes.string.isRequired
+	};
+
+
+	render(){
+		return(
+			<div className="bookshelf">
+             <h2 className="bookshelf-title">{this.props.title}</h2>
+             <div className="bookshelf-books">
+             <ol className="books-grid">{this.props.books.map((book) => (
+                 <li key={book.id} className="item">
+                  <BookList
+                   updateShelf={this.props.updateShelf}
+                   book={book} />
+                     </li>))}
+                     </ol>
+                     </div>
+                     </div>
+                         )
+	}
+             
+	}
+
+	export default BooksShelf;
