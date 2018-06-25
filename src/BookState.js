3c36cad747e5b8d912889ad82ade406d97b589d5
Vismaya T Surendran <vismayatsurendran@gmail.com>
2018-06-25 14:14:55 +0530

    Added the state of book

--- src/BookState.js
+++ src/BookState.js
@@ -1,2 +1,50 @@
 import React, { Component } from 'react'
 import { PropTypes }  from 'prop-types'
+
+class BookState extends Component{
+
+	static PropTypes={
+		updateShelf: PropTypes.func.isRequired
+		book: PropTypes.object.isRequired,
+	};
+
+	state={
+		thisShelf: this.props.book.shelf,
+		change:false
+	};
+}
+
+componentWillReceiveProps(){
+        
+        this.setState({
+            change: false
+        });
+    
+updateShelf = (e) => {
+        this.props.updateShelf(this.props.book, e.target.value);
+        this.setState({
+            thisShelf: e.target.value,
+            change: true
+        });
+    };
+
+
+render(){
+	return(
+           
+           <div className="book-shelf-changer">
+            <select value={this.state.thisShelf} onChange={this.updateShelf}>
+		    <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+             <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+               <option value="none">None</option>
+                </select>
+                </div>
+             )
+         }
+                 
+}
+
+
+export default BookState;
