f76a8ec322adda611319e7f1be6e5e652b6d88bb
Vismaya T Surendran <vismayatsurendran@gmail.com>
2018-06-25 14:28:19 +0530

    Update the book list

---
+++ src/BookList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BookState from './BookState';
+
+class BookList extends Component{
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        updateShelf: PropTypes.func.isRequired
+    };
+
+     render(){
+        const { book } = this.props;
+        return(
+            <div className="book" id={book.id}>
+                <div className="book-top">
+                    <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}>
+                    </div>
+                    <BookState
+                        book={book}
+                        updateShelf={this.props.updateShelf}/>
+                </div>
+                <div className="book-title">{book.name}</div>
+                <div className="book-authors">{book.authors}</div>
+            </div>
+        )
+    }
+}
